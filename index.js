8259b596ed1b099c0c9130f025d7181581270108
Oleksii <oleksii.y@peeklogic.com>
2022-06-19 18:25:35 +0300

    continue backend

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 7000;
+const publicPath = path.join(__dirname, './');
+
+const app = express();
+
+app.use(express.static(publicPath));
+app.get('/', (req, res) => {
+    res.sendFile(publicPath);
+});
+
+app.listen(port, () => {
+    console.log(`listen on port: ${port}`);
+});
