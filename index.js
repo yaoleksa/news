971cecf9919d74b75f87b8f1fef2d5ba6e58ed1d
Oleksii <oleksii.y@peeklogic.com>
2022-06-25 14:48:49 +0300

    p

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 const port = process.env.PORT || 7000;
 const publicPath = path.join(__dirname, './');
 
 const app = express();
 
 app.use(express.static(publicPath));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(publicPath);
