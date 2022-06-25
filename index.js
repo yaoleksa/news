05a04aebfcc4575f9d441def0b21e40534a1dfa1
Oleksii <oleksii.y@peeklogic.com>
2022-06-25 15:10:40 +0300

    good

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors');
 const port = process.env.PORT || 7000;
 const publicPath = path.join(__dirname, './');
 
 const app = express();
 
 app.use(express.static(publicPath));
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(publicPath);
 });
 app.post('/', (req, res) => {
-    fs.writeFile('./test.txt', 'test');
+    fs.writeFile('./test.txt', 'test', (err) => {
+        console.log(err);
+    });
 });
 
 app.listen(port, () => {
