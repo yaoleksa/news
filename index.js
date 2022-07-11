f8cd2e316aa437af634c9eb43a0c863fd74e8f6f
Oleksii <oleksii.y@peeklogic.com>
2022-07-11 15:10:27 +0300

    Work with posting

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 7000;
 const publicPath = path.join(__dirname, './');
 
 const app = express();
 
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/', (req, res) => {
     res.sendFile(publicPath);
 });
+app.get('/policy', (req, res) => {
+    res.sendFile(path.join(__dirname, './myOwnArticles/test.txt'));
+})
 app.post('/', (req, res) => {
-    fs.writeFile('./myOwnArticles/test.txt', 'test', (err) => {
+    console.log({...req.body});
+    const reqObj = {...req.body};
+    fs.writeFile('./myOwnArticles/test.txt', reqObj.name, (err) => {
         console.log(err);
     });
+    res.send('not empty');
 });
 
 app.listen(port, () => {
-    console.log(`listen on port: ${port}`);
+    console.log(`http://localhost:${port}`);
 });
